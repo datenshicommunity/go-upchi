69f018488118dcfe3ad4c230c654229cd3a72fbc
ocy-darma <imadeocy@itsecasia.com>
2024-06-10 16:40:55 +0700

    feat: add endpoint and use gofiber as web framework api

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-    "go-upchi/config"
-    "go-upchi/s3client"
+	"go-upchi/config"
+	"go-upchi/s3client"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -19,6 +22,20 @@ func main() {
     // Create a new S3 session
     svc := s3client.CreateS3Session(accessKeyID, secretAccessKey, region, customEndpoint)
 
-    // List the objects in the specified bucket
-    s3client.ListBucketObjects(svc, bucket, customEndpoint)
+    // Fiber instance
+    app := fiber.New()
+
+    // Route
+    app.Get("/list", func(c *fiber.Ctx) error {
+        objects, err := s3client.ListBucketObjects(svc, bucket, customEndpoint)
+        if err != nil {
+            return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+                "error": err.Error(),
+            })
+        }
+        return c.JSON(objects)
+    })
+
+    // Listen on port 3000
+    log.Fatal(app.Listen(":3000"))
 }
--- s3client/s3list.go
+++ s3client/s3list.go
@@ -2,27 +2,44 @@ package s3client
 
 import (
     "fmt"
-    "log"
+    "time"
 	"github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/service/s3"
 )
 
+// S3Object holds information about an object stored in S3
+type S3Object struct {
+    Name         string    `json:"name"`
+    Size         int64     `json:"size"`
+    LastModified time.Time `json:"last_modified"`
+    URL          string    `json:"url"`
+}
+
 // ListBucketObjects lists all objects in the specified S3 bucket
-func ListBucketObjects(svc *s3.S3, bucket, customEndpoint string) {
+func ListBucketObjects(svc *s3.S3, bucket, customEndpoint string) ([]S3Object, error) {
     input := &s3.ListObjectsV2Input{
         Bucket: aws.String(bucket),
     }
 
     result, err := svc.ListObjectsV2(input)
     if err != nil {
-        log.Fatalf("Unable to list items in bucket %q, %v", bucket, err)
+        return nil, fmt.Errorf("unable to list items in bucket %q, %v", bucket, err)
     }
 
-    fmt.Println("Objects in bucket", bucket)
+    var objects []S3Object
+
     for _, item := range result.Contents {
         url := ConstructObjectURL(customEndpoint, bucket, *item.Key)
-        fmt.Printf("Name: %s, Size: %d, LastModified: %s, URL: %s\n", *item.Key, *item.Size, item.LastModified, url)
+        obj := S3Object{
+            Name:         *item.Key,
+            Size:         *item.Size,
+            LastModified: *item.LastModified,
+            URL:          url,
+        }
+        objects = append(objects, obj)
     }
+
+    return objects, nil
 }
 
 // ConstructObjectURL constructs the URL for an S3 object given the endpoint, bucket, and object key
